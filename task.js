84b644b543aa78ae2cb658403b75e4717203f294
bolaji ogidan <bolajiscott@yahoo.com>
2020-03-24 00:12:59 +0100

    even-numbers

---
+++ task.js
@@ -0,0 +1,19 @@
+let courses=["html","css","js","go-lang"];
+   let name="bolaji-ogidan";
+  console.log(courses);
+  console.log(name);
+
+  //this section checks for even number 
+  let i=4; 
+  for(e = 4; i <= 200; i++)
+  {
+      e = 4;
+      if( e% 2 == 0)
+      {
+          console.log(i);
+      }
+  }
+
+
+  
+
